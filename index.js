ea0c05bc8bea549d08a9c0d377c9c3f1d89d8ad1
tianyisunn <tianyi_sun0611@163.com>
2025-11-14 18:49:41 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//dark-bright mode swap
+const moonIcon = document.getElementById("icon-moon")
+const sunIcon = document.getElementById("icon-sun")
+const themeSwap = document.getElementById("theme-swap")
+
+function changeTheme(mode){
+  if(mode === "dark"){
+    moonIcon.style.display = 'none'
+    sunIcon.style.display = 'block'
+    body.classList.add = 'dark-theme'
+    localStorage.setItem = "theme","dark"
+  }
+  else{
+    moonIcon.style.display = 'block'
+    sunIcon.style.display = 'none'
+    body.classList.remove = 'dark-theme'
+    localStorage.setItem = "theme","bright"
+  }
+}
+//check previous theme
+const savedTheme = localStorage.getItem('theme')
+if(theme === 'dark'){
+  changeTheme('dark')
+}
+  else{
+    changeTheme('bright')
+  }
+
+themeSwap.addEventListener('click',()=>{
+ const currentTheme = body.classList.contains('dark-theme')
+ if(currentTheme){
+  changeTheme(dark)
+ }else{
+  changeTheme(bright)
+ }
+})
